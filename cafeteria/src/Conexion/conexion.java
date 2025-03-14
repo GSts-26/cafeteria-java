f98ffde6eed4e6ad7b447faf58215c1f30813e76
SENA <SENA@CUCDFPRNSFSP109.FormacionSena.RED>
2025-03-14 10:32:59 -0500

    conexion a la base de datos agregada

--- cafeteria/src/Conexion/conexion.java
+++ cafeteria/src/Conexion/conexion.java
@@ -1,5 +1,7 @@
 package Conexion;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -8,25 +10,19 @@ public class conexion {
 
     private Connection conect;
 
+    private HikariDataSource dataSource;
+
     public conexion() {
-        try {
-            Class.forName("org.postgresql.Driver");
-        } catch (Exception e) {
-            System.err.println("Error " + e);
-        }
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl("jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:5432/postgres");
+        config.setUsername("postgres.pyhqsjvywazyifwdeyax");
+        config.setPassword("afeteria_1234");
+        config.setMaximumPoolSize(10); // Puedes ajustar el tamaño del pool según lo necesario
+
+        dataSource = new HikariDataSource(config);
     }
 
-    public Connection getConnection() {
-        try {
-            return conect
-                    = conect = DriverManager.getConnection(
-                            "jdbc:postgresql://aws-0-sa-east-1.pooler.supabase.com:5432/postgres",
-                            "postgres.pyhqsjvywazyifwdeyax", // Usuario
-                            "afeteria_1234"); // Contraseña
-
-        } catch (SQLException e) {
-            System.err.println("Error al obtener la conexion " + e.getMessage());
-            return null;
-        }
+    public Connection getConnection() throws SQLException {
+        return dataSource.getConnection();
     }
 }
