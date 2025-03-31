34e82ab3d88af6999213c02b036bac1b7e74a498
Admin <Admin@DESKTOP-N98LLFN>
2025-03-31 06:58:31 -0500

    modificado

---
+++ proyect_cafeteria/src/modelos/Entidades/usuario.java
@@ -0,0 +1,48 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package modelos.Entidades;
+
+
+/**
+ *
+ * @author Admin
+ */
+
+public class usuario  {
+
+    private String nombre;
+    private String clave;
+    private String rol;
+    public usuario(String nombre, String clave, String rol) {
+        this.nombre = nombre;
+        this.clave = clave;
+        this.rol = rol;
+    }
+
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
+    public String getRol() {
+        return rol;
+    }
+
+    public void setRol(String rol) {
+        this.rol = rol;
+    }
+}
